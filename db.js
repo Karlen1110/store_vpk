38f4ed65fa12b246d37663b2da6013646340d0f1
Karlen Grigoryan <karlen318@gmail.com>
2023-10-16 18:05:47 +0300

    123

--- db.js
+++ db.js
@@ -1,5 +1,3 @@
 const { Sequelize } = require("sequelize");
 
-module.exports = new Sequelize(
-  "postgres://maxim:Wj86sH0xiFkU7wZwwnjmFI4x6XAkHnF0@dpg-ckmk69ou1l6c739tvcs0-a.oregon-postgres.render.com/storesdb?ssl=true"
-);
+module.exports = new Sequelize(process.env.DB_LINK);
